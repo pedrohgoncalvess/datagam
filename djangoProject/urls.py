922801cef80f20f1df9d6731bc0b148b6bfc0ad5
Pedro H <pedro_gonsalves@hotmail.com>
2023-01-26 10:09:50 -0300

    first views

---
+++ djangoProject/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('comercial',views.comercial,name='comercial'),
+    path('hospitalar',views.comercial,name='hospitalar'),
+    path('farma',views.comercial,name='farma'),
+
+]
---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'./home.html')
+
+def comercial(request):
+    return render(request,'comercial.html')
+
+def farma(request):
+    return render(request,'hospitalar.html')
+
+def hospitalar(request):
+    return render(request,'farma.html')
+
